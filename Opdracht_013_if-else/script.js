05b25136dce1d31e695694620de219fc85dbb758
MiekeNap <84043094+MiekeNap@users.noreply.github.com>
2021-06-05 14:49:38 +0200

    Add files via upload

---
+++ Opdracht_013_if-else/script.js
@@ -0,0 +1,32 @@
+
+//Deel 1
+const myAge = 15;
+
+if (myAge > 17) {
+    console.log("je mag naar binnen");
+} else {
+    console.log("sorry, je bent nog te jong dus mag je niet naar binnen");
+}
+
+//Deel 2
+const isFemale = false;
+
+    if (isFemale) {
+        console.log("yes, je bent welkom op de ladiesnight")
+    } else {
+        console.log("sorry, alleen dames zijn vanavond welkom")
+    }
+
+
+//Deel 3
+    const driverStatus = "noDrinks";
+    switch (driverStatus) {
+        case "noDrinks":
+            console.log("je bent de bob en mag rijden");
+            break;
+        case "drinks":
+            console.log("je hebt gedronken en bent dus geen bob");
+    }   
+   
+
+    
